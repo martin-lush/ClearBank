f4cd620db83e3cd3001385739d153015e7a66c74
Martin Lush <martin-hornsby@ntlworld.com>
2021-01-13 19:52:45 +0000

    Add config class for DataStoreType

---
+++ ClearBank.DeveloperTest/Config/AccountDataStoreConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+
+namespace ClearBank.DeveloperTest.Config
+{
+    public interface IAccountDataStoreConfig
+    {
+        string DataStoreType { get; }
+    }
+
+    public class AccountDataStoreConfig : IAccountDataStoreConfig
+    {
+        public string DataStoreType => ConfigurationManager.AppSettings["DataStoreType"];
+    }
+}
