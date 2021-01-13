eed63267f2a429f47b0271dc6d0475184ba321a3
Martin Lush <martin-hornsby@ntlworld.com>
2021-01-13 19:53:30 +0000

    Create helper class to get appropriate datastore

---
+++ ClearBank.DeveloperTest.Tests/Unit/Helpers/AccountDataStoreHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using ClearBank.DeveloperTest.Config;
+using ClearBank.DeveloperTest.Data;
+using ClearBank.DeveloperTest.Helpers;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace ClearBank.DeveloperTest.Tests.Unit.Helpers
+{
+    public class AccountDataStoreHelperTests
+    {
+        [TestCase("Backup", typeof(BackupAccountDataStore))]
+        [TestCase("", typeof(AccountDataStore))]
+        [TestCase(null, typeof(AccountDataStore))]
+        public void GetAccountDataStore_Returns_Expected_AccountDataStore(string dataStoreType, Type expectedType)
+        {
+            // Arrange
+            var dataStoreConfig = new Mock<IAccountDataStoreConfig>();
+            dataStoreConfig.SetupGet(a => a.DataStoreType).Returns(dataStoreType);
+
+            // Act
+            var accountDataStore = AccountDataStoreHelper.GetAccountDataStore(dataStoreConfig.Object);
+
+            // Assert
+            Assert.IsAssignableFrom(expectedType, accountDataStore);
+        }
+    }
+}
---
+++ ClearBank.DeveloperTest/Helpers/AccountDataStoreHelper.cs
@@ -0,0 +1,22 @@
+﻿using ClearBank.DeveloperTest.Config;
+using ClearBank.DeveloperTest.Data;
+
+namespace ClearBank.DeveloperTest.Helpers
+{
+    public static class AccountDataStoreHelper
+    {
+        public static IAccountDataStore GetAccountDataStore(IAccountDataStoreConfig dataStoreConfig)
+        {
+            var dataStoreType = dataStoreConfig.DataStoreType;
+
+            switch (dataStoreType)
+            {
+                // TECH DEBT: Consider using Enums for this
+                case "Backup":
+                    return new BackupAccountDataStore();
+                default:
+                    return new AccountDataStore();
+            }
+        }
+    }
+}
